1aa9c961e6b88c6bbbe3161f9ca5340c482fd22f
Ganishka7 <154536827+Ganishka7@users.noreply.github.com>
2025-01-18 11:23:10 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var sidenavbar = document.querySelector(".snavbar")
+var togglebar = document.querySelector(".toggle")
+
+
+
+function openNav(){
+    sidenavbar.style.left="0"
+
+    
+}
+
+function closeNav(){
+    sidenavbar.style.left="-60%"
+}
+
+
